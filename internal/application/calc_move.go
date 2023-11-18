33a238193aceb33bdd0fe54ce0d60f3f76644503
Elliot Brookes <elliotbrookes1@gmail.com>
2023-11-18 21:50:08 +0000

    application files, no functionality yet

---
+++ internal/application/calc_move.go
@@ -0,0 +1,8 @@
+package application
+
+import "ElliotBrookes/move-manager/internal/domain"
+
+func CalcMove(domain.MoveData) error {
+	//
+	return nil
+}
---
+++ internal/application/help.go
@@ -0,0 +1,6 @@
+package application
+
+func CliHelp(a []string) (string, error) {
+	// Run help, with additional flags changing the output
+	return "Help string example", nil
+}
