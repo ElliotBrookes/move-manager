cb69feb3e2bbd6842c00e0226502fa412c47dbcf
Elliot Brookes <elliotbrookes1@gmail.com>
2023-11-27 16:51:46 +0800

    added in test files for the application layer, will write logic in later

--- internal/application/add-journey.go
+++ internal/application/add-journey.go
@@ -1,3 +1,10 @@
 package application
 
 // Code for creating a journey in the move
+
+type AddJourneyAppInstance struct{}
+
+func (i AddJourneyAppInstance) Execute() error {
+
+	return nil
+}
---
+++ internal/application/add-journey_test.go
@@ -0,0 +1,7 @@
+package application
+
+import "testing"
+
+func TestAddJourneyAppInstance(t *testing.T) {
+	// Test code for the Execute functionality of the add journey instance
+}
